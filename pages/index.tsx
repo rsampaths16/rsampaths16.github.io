b13b2537a3e7c8886fab3930cb61cda16b25be11
rsampaths16 <rsampaths16@gmail.com>
2022-01-31 10:56:46 +0530

    Add a home page
    Issues:
    * https://github.com/rsampaths16/rsampaths16.github.io/issues/1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
-import type { NextPage } from 'next'
+import Layout from 'components/layout'
+import Container from 'components/container'
+import QuickIntro from 'components/quick-intro'
 
-const Home: NextPage = () => {
-  return (<div/>)
+
+export default function Home(): JSX.Element {
+  const avatarURL = '/images/avatar.jpg'
+  const greeting = 'Hey, I\'m Sampath'
+  const caption = 'Software Engineer'
+  const introLines: string[] = [
+    'I hail from 90s India',
+    'I Code, watch Anime & read Manga',
+    'I like Orange & Black'
+  ]
+  const postScript = 'P.S: Read One-Piece'
+
+  return (
+    <Layout>
+      <Container>
+        <QuickIntro
+          avatarUrl={avatarURL}
+          greeting={greeting}
+          caption={caption}
+          introLines={introLines}
+          postScript={postScript}
+        />
+      </Container>
+    </Layout>
+  )
 }
 
-export default Home
